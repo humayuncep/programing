6db1619650c081d04a86c3f2c71358b949795938
Humayun <humayunkabir.cep@gmail.com>
2018-04-29 19:16:53 +0600

    Learning Node.js, 2/e Lesson: 1 Complete

---
+++ nodejs/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+let process_request = (req, res) => {
+  let body = 'Thanks for calling!\n';
+  let content_length = body.length;
+
+  res.writeHead(200, {
+    'Content-Length': content_length,
+    'Content-Type': 'text/plain',
+  });
+
+  res.end(body);
+}
+
+let s = http.createServer(process_request);
+s.listen(8080);
