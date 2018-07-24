e426485530e6cc8ad1253ce8b01c316e153c6db4
Humayun kabir <humayunkabir.cep@gmail.com>
2018-07-25 02:02:29 +0600

    Add cars

--- Games/Car/assets/js/lib.js
+++
@@ -1,9 +0,0 @@
-export const calc = () => ({
-  multiply: (a, b) => a * b,
-  divide: (a, b) => a / b,
-  add: (a, b) => a + b,
-  subtract: (a, b) => a - b,
-});
-export const sqrt = Math.sqrt;
-export const square = x => x * x;
-export const  diag = (x, y) => sqrt(square(x) + square(y));
