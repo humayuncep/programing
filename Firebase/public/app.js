eb87475c85ac79e765ad5e1350aa70c3f9b4b08b
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-07-26 21:11:03 +0600

    Cars

--- Firebase/public/app.js
+++ Firebase/public/app.js
@@ -1,39 +1,39 @@
 document.addEventListener('DOMContentLoaded', event => {
   const app = firebase.app();
   const db = firebase.firestore();
-  const myPost = db.collection('posts').doc('firstpost');
-  const productsRef = db.collection('products');
-  const query = productsRef.where('items', '>', 5);
-  let output = '';
-  query.get()
-    .then(products => {
-      products.forEach(doc => {
-        const data = doc.data();
-        output += `Name: ${data.name} and Items: ${data.items} <br/>`;
-        document.querySelector('#products').innerHTML = output;
-      })
-    })
-    .catch(console.log);
-  
-  
-  myPost.onSnapshot(doc => {
-    const data = doc.data();
-    console.log(data);
-    document.querySelector('#title').innerHTML = data.title;
-  });
+  // const myPost = db.collection('posts').doc('firstpost');
+  // const productsRef = db.collection('products');
+  // const query = productsRef.where('items', '>', 5);
+  // let output = '';
+  // query.get()
+  //   .then(products => {
+  //     products.forEach(doc => {
+  //       const data = doc.data();
+  //       output += `Name: ${data.name} and Items: ${data.items} <br/>`;
+  //       document.querySelector('#products').innerHTML = output;
+  //     })
+  //   })
+  //   .catch(console.log);
+  //
+  //
+  // myPost.onSnapshot(doc => {
+  //   const data = doc.data();
+  //   console.log(data);
+  //   document.querySelector('#title').innerHTML = data.title;
+  // });
 });
 
 
 /*-----------------------------------------------
 |   Update Post
 -----------------------------------------------*/
-function updatePost(e) {
-  const db = firebase.firestore();
-  const myPost =  db.collection('posts').doc('firstpost');
-  myPost.update({
-    title: e.target.value,
-  })
-}
+// function updatePost(e) {
+//   const db = firebase.firestore();
+//   const myPost =  db.collection('posts').doc('firstpost');
+//   myPost.update({
+//     title: e.target.value,
+//   })
+// }
 
 
 /*-----------------------------------------------
@@ -44,8 +44,12 @@ function googleLogin() {
   firebase.auth().signInWithPopup(provider)
     .then( result => {
       const user = result.user;
+      console.log(result.additionalUserInfo);
+      const welcomeTemplate = `
+        <h1>Welcome back ${user.displayName}</h1>
+        
+      `;
       document.write(`Hello, ${user.displayName}`);
-      console.log(result);
     })
     .catch(console.log);
 }
